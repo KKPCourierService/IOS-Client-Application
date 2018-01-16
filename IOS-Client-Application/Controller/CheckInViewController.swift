735b6870ed7ca22d28542d38dc31cac3e48ab35d
Игорь Коршунов <kroshigor@mail.ru>
2018-01-17 04:09:39 +0700

    Добавил окна авторизации и регистрации

---
+++ IOS-Client-Application/Controller/CheckInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CheckInViewController.swift
+//  IOS-Client-Application
+//
+//  Created by Игорь Коршунов on 17.01.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+}
---
+++ IOS-Client-Application/Controller/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  IOS-Client-Application
+//
+//  Created by Игорь Коршунов on 17.01.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+ 
+
+}
