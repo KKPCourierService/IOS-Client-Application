287603e63d9a469df42772e9fe6af11784b5d665
Игорь Коршунов <kroshigor@mail.ru>
2018-01-24 08:29:08 +0700

    Изменено наименование форм неавторизованного пользователя в соответствии с их значением

--- IOS-Client-Application/Controller/ScreenOfAnUnuthorizedUser/MainUnuthorizedViewController.swift
+++ IOS-Client-Application/Controller/ScreenOfAnUnuthorizedUser/MainUnuthorizedViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainUnuthorizedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
