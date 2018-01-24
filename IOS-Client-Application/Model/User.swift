5bc03bd6096c9aadd0ca9d3c78a068d601c90467
Игорь Коршунов <kroshigor@mail.ru>
2018-01-24 08:32:39 +0700

    Объявлен класс описывающий пользователя

---
+++ IOS-Client-Application/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  IOS-Client-Application
+//
+//  Created by Игорь Коршунов on 24.01.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class User {
+    
+}
