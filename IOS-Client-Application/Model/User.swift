d12baa4102a7d0070d5a97b26e6a8dc32548144c
Игорь Коршунов <kroshigor@mail.ru>
2018-01-24 08:50:26 +0700

    Описаны поля класса пользователя и инициализатор класса

--- IOS-Client-Application/Model/User.swift
+++ IOS-Client-Application/Model/User.swift
@@ -10,5 +10,23 @@ import Foundation
 import Alamofire
 
 class User {
+    private var _id: Int?
+    private var _name: String
+    private var _surname: String
+    private var _patronymic: String
+    private var _email: String
+    private var _password: String
+    private var _phoneNumber: String
+    private var _photoURL: String?
     
+    private init (id: Int, name: String, surname: String, patronymic: String, email: String, password: String, phoneNumber: String, photoURL: String?) {
+        _id = id
+        _name = name
+        _surname = surname
+        _patronymic = patronymic
+        _email = email
+        _password = password
+        _phoneNumber = phoneNumber
+        _photoURL = photoURL
+    }
 }
