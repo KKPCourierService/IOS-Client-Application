7f8ca198db7a36d87f2c9232a77a7d0475c76cdb
Igor <korshunigot01@gmail.com>
2018-05-13 00:46:10 +0700

    Объявлен ViewModel для меню пользователя

---
+++ IOS-Client-Application/ViewModel/AuthorizedUser/SideMenuViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SideMenuViewModel.swift
+//  IOS-Client-Application
+//
+//  Created by Игорь Коршунов on 13.05.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class SideMenuViewModel {
+    
+}
