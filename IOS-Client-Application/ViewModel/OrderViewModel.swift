851671ee3ad1008abb9009568e9be44f89430336
Igor <korshunigot01@gmail.com>
2018-05-18 13:02:54 +0700

    Объявлен ViewModel для заказов

---
+++ IOS-Client-Application/ViewModel/OrderViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OrderViewModel.swift
+//  IOS-Client-Application
+//
+//  Created by Игорь Коршунов on 18.05.2018.
+//  Copyright © 2018 Игорь Коршунов. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class OrderViewModel {
+    
+}
